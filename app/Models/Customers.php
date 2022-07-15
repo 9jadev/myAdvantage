3d2fb9498a609cae4e527a43aeef4f2599b81146
Ahamba Solomon <solomon.ahamba@botosoft.com>
2022-07-15 13:01:33 +0100

    sdimk  dmksm, in

--- app/Models/Customers.php
+++ app/Models/Customers.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use App\Models\Documents as ModelsDocuments;
-use App\models\Kyc;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
