b164032ff6d422f8eb650707ab094e9777157ed8
Ahamba Solomon <solomon.ahamba@botosoft.com>
2022-07-21 19:42:30 +0100

    sdvsd

--- app/Services/MyIdService.php
+++ app/Services/MyIdService.php
@@ -2,27 +2,36 @@
 
 namespace App\Services;
 
-use App\Models\Post;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class MyIdService
 {
     // Declare the function as static
     public static function verify(string $slug)
     {
-        $response = Http::withHeaders([
-            'x-api-key' => env("ID_PASS_SECRET"),
-            'X-Second' => env('ID_APP_ID'),
-        ])->post(env("ID_BASE_URL") . '/api/v2/biometrics/merchant/data/verification/bvn', [
-            'number' => $slug,
-        ]);
-        $resData = $response->json();
-        if ($resData["status"] == true) {
-            $resData["message"] = "Verified successfully";
-            return $resData;
-        } else {
+
+        try {
+            $response = Http::withHeaders([
+                'x-api-key' => env("ID_PASS_SECRET"),
+                'X-Second' => env('ID_APP_ID'),
+            ])->post(env("ID_BASE_URL") . '/api/v2/biometrics/merchant/data/verification/bvn', [
+                'number' => $slug,
+            ]);
+            $resData = $response->json();
+            if ($resData["status"] == true) {
+                $resData["message"] = "Verified successfully";
+                return $resData;
+            } else {
+                $resData["message"] = "Error Occoured";
+                return $resData;
+            }
+
+        } catch (\Throwable$th) {
+            Log::error($th);
             $resData["message"] = "Error Occoured";
             return $resData;
+
         }
 
     }
