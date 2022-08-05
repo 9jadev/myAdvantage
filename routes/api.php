6ba1bd2dd02303cd1c0cf8373a36e3a42be59119
Ahamba Solomon <solomon.ahamba@botosoft.com>
2022-08-05 09:11:02 +0100

    Transactions Web hook

--- routes/api.php
+++ routes/api.php
@@ -82,7 +82,7 @@ Route::prefix('v1')->group(function () {
             Route::post('fund', [TransactionsController::class, 'create'])->middleware(['auth:sanctum', 'type.customer']);
             Route::get('verify', [TransactionsController::class, 'verifyPayments'])->middleware(['auth:sanctum', 'type.customer']);
             Route::get('banks', [TransactionsController::class, 'listBanks'])->middleware(['auth:sanctum', 'type.customer']);
-            Route::get('webhook', [TransactionsController::class, 'paymentWebhook']);
+            Route::post('webhook', [TransactionsController::class, 'paymentWebhook']);
             Route::get('transactions', [TransactionsController::class, 'index'])->middleware(['auth:sanctum', 'type.customer']);
 
             Route::prefix('transfer')->group(function () {
