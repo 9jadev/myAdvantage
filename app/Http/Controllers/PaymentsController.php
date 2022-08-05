78445669b64f6923952297c468a75dffb2bb7d0f
Ahamba Solomon <solomon.ahamba@botosoft.com>
2022-08-05 15:16:18 +0100

    Sofie

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -37,8 +37,13 @@ class PaymentsController extends Controller
     public function customerPaymentList()
     {
         $page_number = request()->input("page_number");
+        $status = request()->input("status");
         $customer = auth()->user();
-        $payments = Payments::where("status", "1")->where("customer_id", $customer->customer_id)->latest()->paginate($page_number);
+        $payments = Payments::where("customer_id", $customer->customer_id)->latest();
+        if ($status) {
+            $payments->where("status", $status);
+        }
+        $payments = $payments->paginate($page_number);
         return response()->json(["message" => "Payments list", "payments" => $payments, "status" => "success", "desc" => "status description 0 awating payment , 1 completed 2 canceled"], 200);
     }
 
