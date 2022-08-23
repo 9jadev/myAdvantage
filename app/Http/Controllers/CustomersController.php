1c4fb02c2fc9070fcd165026db499a6f49f1ed68
Ahamba Solomon <solomon.ahamba@botosoft.com>
2022-08-23 08:39:03 +0100

    andimk

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -269,9 +269,10 @@ class CustomersController extends Controller
             $response = Http::withHeaders([
                 'Content-Type' => 'application/json',
                 'Authorization' => "Bearer " . env('FWAVE_PRIVATE_KEY'),
-            ])->get(env("FWAVE_BASE") . '/v3/transactions/verify_by_reference?tx_ref=' . $ref);
+            ])->get(env("FWAVE_BASE") . '/transactions/verify_by_reference?tx_ref=' . $ref);
             $responseData = $response->json();
-            // return $responseData["status"];
+            // return 12324;
+            // return $responseData;
             if ($responseData["status"] == "error") {
                 return response()->json([
                     "message" => "Invalid transaction",
