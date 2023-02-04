708443f6179684554cd7d577c0f3bed17ec6cb41
Ahamba Solomon <solomon.ahamba@botosoft.com>
2023-02-04 19:35:03 +0100

    Stanky

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,6 +4,15 @@ sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 
 php-fpm -D
 
+echo ">>Running migration..."
+php artisan migrate --force
+echo ">>Running seeding..."
+php artisan db:seed --force
+echo ">>Publishing vendor assets..."
+php artisan vendor:publish --tag=public --force
+echo ">>Linking storage..."
+php artisan storage:link
+
 while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
 
 nginx
